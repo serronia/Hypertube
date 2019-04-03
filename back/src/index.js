51a92beae5f656ed28fe6a47383564fc0c2e7b4d
Drakauf <shthevak@z2r1p1.le-101.fr>
2019-04-03 13:15:09 +0200

    [docker-compose] => Server Done : -listening on 8080 -nodemon

--- back/src/index.js
+++ back/src/index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var session = require('express-session');
 var app = express();
-var port = process.env.SERVER_PORT;
+var port = process.env.P_BACK;
 
 var http = require('http').Server(app);
 
 app.use('/', function(req, res) {
-	console.log("server hit");
+		console.log("server hitted");
 });
 
 http.listen(port, function(){
-	  console.log('server listening on port :' + port);
+		  console.log('server listening on port : ' + port);
 });
+
