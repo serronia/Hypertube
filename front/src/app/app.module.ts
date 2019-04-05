7c5faacfce25e1f2c63603852621a5b34d3b48c9
Unknown <mchapard@student.le-101.fr>
2019-04-05 14:56:25 +0200

    miou

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ChatComponent } from './components/chat/chat.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    HeaderComponent,
+    FooterComponent,
+    PrincipalComponent
   ],
   imports: [
   BrowserModule
