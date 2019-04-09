4f5bad225350bc20c862abc999a13da8f70ba125
jdarko <jevtic.darko.3@gmail.com>
2019-04-09 12:03:07 +0200

    modification du footer, header, suppression du dossier -component-

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { ChatComponent } from './components/chat/chat.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { PrincipalComponent } from './components/principal/principal.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent,
     HeaderComponent,
     FooterComponent,
-    PrincipalComponent
   ],
   imports: [
   BrowserModule
